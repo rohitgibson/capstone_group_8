883b80d4fda4109597e88346060f0d2d696d5f1b
Rohit Gibson <89996347+rohitgibson@users.noreply.github.com>
2023-11-18 21:42:01 -0500

    Removal of AVS UI code from main branch.
    Removed b/c of decision to refocus work towards API testing, deployment, and documentation. (Still on local)

--- avs_ui/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, make_response, jsonify
-import simplejson as json
-
-from components.dynamicComponents import DynamicComponents
-
-app = Flask(__name__)
-
-dynamicComponents = DynamicComponents()
-
