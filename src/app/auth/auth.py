06669cca5e549fbd9517223ff6e9eff0012f7829
rohitgibson <89996347+rohitgibson@users.noreply.github.com>
2023-11-25 18:13:39 -0500

    Updates to auth

--- src/app/auth/auth.py
+++ src/app/auth/auth.py
@@ -11,17 +11,20 @@ class HTTPBasicAuth():
         self.authVerification = AuthVerification()
 
     def authUser(self, permitted_roles:list[str], auth_data):
-        if auth_data is not None and type(auth_data) == dict:
-            auth_data = dict(auth_data)
-            role = self.authVerification.checkUserCredentials(username=auth_data["username"], password=auth_data["password"])
-        elif auth_data is not None and type(auth_data) == str:
-            auth = str(auth_data).split(" ")[1]
-            auth_str = base64.b64decode(auth).decode().split(":")
-            role = self.authVerification.checkUserCredentials(username=auth_str[0], password=auth_str[1])
-        else:
-            abort(403)
+        try:
+            if auth_data is not None and type(auth_data) == dict:
+                auth_data = dict(auth_data)
+                role = self.authVerification.checkUserCredentials(username=auth_data["username"], password=auth_data["password"])
+            elif auth_data is not None and type(auth_data) == str:
+                auth = str(auth_data).split(" ")[1]
+                auth_str = base64.b64decode(auth).decode().split(":")
+                role = self.authVerification.checkUserCredentials(username=auth_str[0], password=auth_str[1])
+            else:
+                abort(403)
 
-        if role not in permitted_roles:
+            if role not in permitted_roles:
+                abort(403)
+            else:
+                pass
+        except Exception:
             abort(403)
-        else:
-            pass
