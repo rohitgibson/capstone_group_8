102c193bb8e50e7727493c3c6d19f8fd47bda335
rohitgibson <89996347+rohitgibson@users.noreply.github.com>
2023-11-25 19:57:23 -0500

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -6,7 +6,7 @@ from blueprints.address import address_blueprint
 from blueprints.users import user_blueprint
 from blueprints.demo import demo_blueprint
 
-logging.basicConfig(filename="/src/app/logs/avs.log", level=logging.DEBUG)
+# logging.basicConfig(filename="/src/app/logs/avs.log", level=logging.DEBUG)
 
 app = Quart(__name__)
 
